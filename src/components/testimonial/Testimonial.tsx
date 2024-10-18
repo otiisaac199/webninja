50fe20acbbbf3803b320ccf974f7876e3c87ea46
otiisaac199 <otichinazaisaac19@gmail.com>
2024-10-18 21:57:15 +0100

    added the slider... still have some ts bugs

--- src/components/testimonial/Testimonial.tsx
+++ src/components/testimonial/Testimonial.tsx
@@ -1,13 +1,264 @@
 import Title from "../layouts/Title";
+import Slider from "react-slick";
+import { testimonialOne, testimonialTwo, quote } from "../../assets";
+import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
+import { RiStarFill } from "react-icons/ri";
+import { ReactNode, useState } from "react";
+
+type Props = {
+  props: {
+    onClick: Function;
+  };
+};
+
+function SampleNextArrow(props: Props) {
+  const { onClick } = props;
+
+  return (
+    <div
+      className="w-14 h-12 bg-[#0c1821] hover:bg-black duration-300 rounded-md text-2xl text-gray-400 flex justify-center items-center absolute top-0 right-0 shadow-shadowOne cursor-pointer z-10"
+      onClick={onClick}
+    >
+      <HiArrowRight />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props: Props) {
+  const { onClick } = props;
+  return (
+    <div
+      className="w-14 h-12 bg-[#0c1821] hover:bg-black duration-300 rounded-md text-2xl text-gray-400 flex justify-center items-center absolute top-0 right-20 shadow-shadowOne cursor-pointer z-10"
+      onClick={onClick}
+    >
+      <HiArrowLeft />
+    </div>
+  );
+}
 
 const Testimonial = () => {
+  const [dotActive, setDotActive] = useState<number>(0);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    beforeChange: (prev, next) => {
+      setDotActive(next);
+    },
+    appendDots: (dots: ReactNode) => (
+      <div
+        style={{
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <ul
+          style={{
+            display: "flex",
+            gap: "15px",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          {" "}
+          {dots}{" "}
+        </ul>
+      </div>
+    ),
+    customPaging: (i: number) => (
+      <div
+        style={
+          i === dotActive
+            ? {
+                width: "12px",
+                height: "12px",
+                color: "blue",
+                background: "#ff014f",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }
+            : {
+                width: "12px",
+                height: "12px",
+                color: "blue",
+                background: "gray",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }
+        }
+      ></div>
+    ),
+  };
+
   return (
     <section
-      id="resume"
+      id="testimonial"
       className="w-full py-20 border-b-[1px] border-b-black "
     >
       <div className="flex justify-center items-center text-center">
         <Title title="WHAT CLIENT SAYS" desc="Testimonial" />
+      </div>
+
+      <div className="max-w-6xl mx-auto">
+        <Slider {...settings}>
+          {/* =========Slider ONE========== */}
+          <div className="w-full">
+            <div className="w-full h-[500px] flex justify-between">
+              <div className="w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne flex flex-col justify-center gap-8 p-8">
+                <img
+                  className="h-72 rounded-lg object-cover"
+                  src={testimonialOne}
+                  alt="testimonialOne"
+                />
+                <div className="w-full flex flex-col justify-end">
+                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
+                    Bound - Trolola
+                  </p>
+                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  <p className="text-base tracking-wide text-gray-500">
+                    Operation Officer
+                  </p>
+                </div>
+              </div>
+              <div className="w-[60%] h-full flex flex-col justify-between">
+                <img className="w-[20%]" src={quote} alt="quote" />
+                <div className="w-full h-[70%] bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne p-8 flex flex-col justify-center gap-8">
+                  <div className="flex justify-between items-center py-6 border-b-2 border-b-gray-900">
+                    <div>
+                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                        Travel Mobile App Design.
+                      </h3>
+                      <p className="text-base text-gray-400 mt-3">
+                        via Upwork - Mar 4, 2015 - Aug 30, 2021 test
+                      </p>
+                    </div>
+                    <div className="text-yellow-500 flex gap-1">
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                    </div>
+                  </div>
+                  <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+                    dolorum, eos natus ipsum numquam veniam officia
+                    necessitatibus ratione quos debitis exercitationem
+                    repudiandae facilis id neque nihil accusantium perspiciatis
+                    repellat? Iste.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* =========Slider TWO========== */}
+          <div className="w-full">
+            <div className="w-full h-[500px] flex justify-between">
+              <div className="w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne flex flex-col justify-center gap-8 p-8">
+                <img
+                  className="h-72 rounded-lg object-cover"
+                  src={testimonialTwo}
+                  alt="testimonialTwo"
+                />
+                <div className="w-full flex flex-col justify-end">
+                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
+                    Bound - Trolola
+                  </p>
+                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  <p className="text-base tracking-wide text-gray-500">
+                    Operation Officer
+                  </p>
+                </div>
+              </div>
+              <div className="w-[60%] h-full flex flex-col justify-between">
+                <img className="w-[20%]" src={quote} alt="quote" />
+                <div className="w-full h-[70%] bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne p-8 flex flex-col justify-center gap-8">
+                  <div className="flex justify-between items-center py-6 border-b-2 border-b-gray-900">
+                    <div>
+                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                        Travel Mobile App Design.
+                      </h3>
+                      <p className="text-base text-gray-400 mt-3">
+                        via Upwork - Mar 4, 2015 - Aug 30, 2021 test
+                      </p>
+                    </div>
+                    <div className="text-yellow-500 flex gap-1">
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                    </div>
+                  </div>
+                  <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+                    dolorum, eos natus ipsum numquam veniam officia
+                    necessitatibus ratione quos debitis exercitationem
+                    repudiandae facilis id neque nihil accusantium perspiciatis
+                    repellat? Iste.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* =========Slider THREE========== */}
+          <div className="w-full">
+            <div className="w-full h-[500px] flex justify-between">
+              <div className="w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne flex flex-col justify-center gap-8 p-8">
+                <img
+                  className="h-72 rounded-lg object-cover"
+                  src={testimonialOne}
+                  alt="testimonialOne"
+                />
+                <div className="w-full flex flex-col justify-end">
+                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
+                    Bound - Trolola
+                  </p>
+                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
+                  <p className="text-base tracking-wide text-gray-500">
+                    Operation Officer
+                  </p>
+                </div>
+              </div>
+              <div className="w-[60%] h-full flex flex-col justify-between">
+                <img className="w-[20%]" src={quote} alt="quote" />
+                <div className="w-full h-[70%] bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne p-8 flex flex-col justify-center gap-8">
+                  <div className="flex justify-between items-center py-6 border-b-2 border-b-gray-900">
+                    <div>
+                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                        Travel Mobile App Design.
+                      </h3>
+                      <p className="text-base text-gray-400 mt-3">
+                        via Upwork - Mar 4, 2015 - Aug 30, 2021 test
+                      </p>
+                    </div>
+                    <div className="text-yellow-500 flex gap-1">
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                      <RiStarFill />
+                    </div>
+                  </div>
+                  <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+                    dolorum, eos natus ipsum numquam veniam officia
+                    necessitatibus ratione quos debitis exercitationem
+                    repudiandae facilis id neque nihil accusantium perspiciatis
+                    repellat? Iste.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Slider>
       </div>
     </section>
   );
