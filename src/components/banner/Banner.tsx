f17ed5298b0c567f7844de4d7c5c9c9306397eb2
otiisaac199 <otichinazaisaac19@gmail.com>
2024-10-21 13:40:46 +0100

    footer added

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <section
       id="home"
-      className="w-full pt-10 pb-20 flex items-center border-b-[1px] border-b-black font-titleFont"
+      className="w-full pt-10 pb-20 flex flex-col gap-10 lgl:flex-row xl:gap-0 items-center border-b-[1px] border-b-black font-titleFont"
     >
       <LeftBanner />
       <RightBanner />
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,112 @@
+import { logo } from "../../assets";
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="w-full py-20 h-96 border-b-[1px] border-b-black grid grid-cols-4 gap-24">
+      <div className="w-full h-full flex flex-col gap-4">
+        <img className="w-32" src={logo} alt="logo" />
+        <div className="flex gap-4">
+          <span className="bannerIcon">
+            <FaFacebookF />
+          </span>
+          <span className="bannerIcon">
+            <FaTwitter />
+          </span>
+          <span className="bannerIcon">
+            <FaLinkedinIn />
+          </span>
+        </div>
+      </div>
+      <div className="w-full h-full">
+        <h3 className="text-xl uppercase text-designColor tracking-wider">
+          Quick Link
+        </h3>
+        <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              About
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              Portfolio
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              Services
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              Contact
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full h-full">
+        <h3 className="text-xl uppercase text-designColor tracking-wider">
+          Resources
+        </h3>
+        <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              System Status
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              Terms of Service
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              Pricing
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full h-full">
+        <h3 className="text-xl uppercase text-designColor tracking-wider">
+          Developers
+        </h3>
+        <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              Documentation
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              Authentication
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              API Reference
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+              Support
+              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
